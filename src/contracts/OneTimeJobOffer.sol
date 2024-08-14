bd59ac12d6e1adf49519228f07514099111f84be
ofalei <oleksii.falei@gmail.com>
2024-08-14 16:42:33 +0700

    Naming

--- src/contracts/OneTimeJobOffer.sol
+++ src/contracts/OneTimeJobOffer.sol
@@ -5,10 +5,10 @@ import "./system-contracts/HederaResponseCodes.sol";
 import "./system-contracts/IHederaTokenService.sol";
 import "./system-contracts/HederaTokenService.sol";
 
-contract OneTimeJobEscrow is HederaTokenService {
+contract OneTimeJobOffer is HederaTokenService {
     // Define an enum for the escrow status
-    enum EscrowStatus { NotDeposited, Deposited, Released, Refunded }
-    EscrowStatus internal status;
+    enum PaymentStatus { NotDeposited, Deposited, Released, Refunded }
+    PaymentStatus internal status;
     address internal immutable employer;
     address internal immutable employee;
     address internal immutable token;
@@ -23,12 +23,12 @@ contract OneTimeJobEscrow is HederaTokenService {
         _;
     }
 
-    modifier inStatus(EscrowStatus _status) {
+    modifier inStatus(PaymentStatus _status) {
         require(status == _status, "Function cannot be called at this stage");
         _;
     }
 
-    constructor(address _employer, address _employee, address _token, int64 _amount) public {
+    constructor(address _employer, address _employee, address _token, int64 _amount) {
         require(_employer != address(0), "Employer address cannot be zero");
         require(_employee != address(0), "Employee address cannot be zero");
         require(_amount > 0, "Amount must be greater than zero");
@@ -37,45 +37,45 @@ contract OneTimeJobEscrow is HederaTokenService {
         employee = _employee;
         token = _token;
         amount = _amount;
-        status = EscrowStatus.NotDeposited;
+        status = PaymentStatus.NotDeposited;
     }
 
     // Function to deposit tokens into escrow by the employer
-    function deposit() external onlyEmployer inStatus(EscrowStatus.NotDeposited) returns (int) {
+    function deposit() external onlyEmployer inStatus(PaymentStatus.NotDeposited) returns (int) {
         int response = HederaTokenService.transferToken(token, employer, address(this), amount);
         require(response == HederaResponseCodes.SUCCESS, "Token transfer to escrow failed");
 
-        status = EscrowStatus.Deposited;
+        status = PaymentStatus.Deposited;
         emit Deposited(employer, amount);
         return response;
     }
 
     // Function to release tokens from escrow to the employee upon successful completion of the work
-    function release() external onlyEmployer inStatus(EscrowStatus.Deposited) returns (int) {
+    function release() external onlyEmployer inStatus(PaymentStatus.Deposited) returns (int) {
         int response = HederaTokenService.transferToken(token, address(this), employee, amount);
         require(response == HederaResponseCodes.SUCCESS, "Token transfer to employee failed");
 
-        status = EscrowStatus.Released;
+        status = PaymentStatus.Released;
         emit Released(employee, amount);
         return response;
     }
 
     // Function to refund tokens back to the employer if the work is not completed
-    function refund() external onlyEmployer inStatus(EscrowStatus.Deposited) returns (int) {
+    function refund() external onlyEmployer inStatus(PaymentStatus.Deposited) returns (int) {
         int response = HederaTokenService.transferToken(token, address(this), employer, amount);
         require(response == HederaResponseCodes.SUCCESS, "Token refund to employer failed");
 
-        status = EscrowStatus.Refunded;
+        status = PaymentStatus.Refunded;
         emit Refunded(employer, amount);
         return response;
     }
 
     function getStatus() external view returns (string memory) {
-        if (status == EscrowStatus.Deposited) {
+        if (status == PaymentStatus.Deposited) {
             return "DEPOSITED";
-        } else if (status == EscrowStatus.Released) {
+        } else if (status == PaymentStatus.Released) {
             return "RELEASED";
-        } else if (status == EscrowStatus.Refunded) {
+        } else if (status == PaymentStatus.Refunded) {
             return "REFUNDED";
         }
         return "NOT_DEPOSITED";
