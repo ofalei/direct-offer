ca964b390ce9a61a32eee6febedb3e185c49b507
ofalei <oleksii.falei@gmail.com>
2024-08-15 22:42:29 +0700

    Updated contracts

--- src/contracts/OneTimeJobOffer.sol
+++ src/contracts/OneTimeJobOffer.sol
@@ -9,6 +9,7 @@ contract OneTimeJobOffer is HederaTokenService {
     // Define an enum for the escrow status
     enum PaymentStatus { NotDeposited, Deposited, Released, Refunded }
     PaymentStatus internal status;
+    address internal immutable operator;
     address internal immutable employer;
     address internal immutable employee;
     address internal immutable token;
@@ -17,22 +18,18 @@ contract OneTimeJobOffer is HederaTokenService {
     event Deposited(address employer, int64 amount);
     event Released(address employee, int64 amount);
     event Refunded(address employer, int64 amount);
-
-    modifier onlyEmployer() {
-        require(msg.sender == employer, "Only employer can call this function");
-        _;
-    }
-
+    
     modifier inStatus(PaymentStatus _status) {
         require(status == _status, "Function cannot be called at this stage");
         _;
     }
 
-    constructor(address _employer, address _employee, address _token, int64 _amount) {
+    constructor(address _operator, address _employer, address _employee, address _token, int64 _amount) {
+        require(_operator != address(0), "Operator address cannot be zero");
         require(_employer != address(0), "Employer address cannot be zero");
         require(_employee != address(0), "Employee address cannot be zero");
         require(_amount > 0, "Amount must be greater than zero");
-
+        operator = _operator;
         employer = _employer;
         employee = _employee;
         token = _token;
@@ -41,7 +38,8 @@ contract OneTimeJobOffer is HederaTokenService {
     }
 
     // Function to deposit tokens into escrow by the employer
-    function deposit() external onlyEmployer inStatus(PaymentStatus.NotDeposited) returns (int) {
+    function deposit() external inStatus(PaymentStatus.NotDeposited) returns (int) {
+        require(msg.sender == employer, "Only employer can call this function");
         int response = HederaTokenService.transferToken(token, employer, address(this), amount);
         require(response == HederaResponseCodes.SUCCESS, "Token transfer to escrow failed");
 
@@ -51,7 +49,8 @@ contract OneTimeJobOffer is HederaTokenService {
     }
 
     // Function to release tokens from escrow to the employee upon successful completion of the work
-    function release() external onlyEmployer inStatus(PaymentStatus.Deposited) returns (int) {
+    function release() external inStatus(PaymentStatus.Deposited) returns (int) {
+        require(msg.sender == operator || msg.sender == employer, "Only operator or employer can call this function");
         int response = HederaTokenService.transferToken(token, address(this), employee, amount);
         require(response == HederaResponseCodes.SUCCESS, "Token transfer to employee failed");
 
@@ -61,7 +60,8 @@ contract OneTimeJobOffer is HederaTokenService {
     }
 
     // Function to refund tokens back to the employer if the work is not completed
-    function refund() external onlyEmployer inStatus(PaymentStatus.Deposited) returns (int) {
+    function refund() external inStatus(PaymentStatus.Deposited) returns (int) {
+        require(msg.sender == operator, "Only operator or employer can call this function");
         int response = HederaTokenService.transferToken(token, address(this), employer, amount);
         require(response == HederaResponseCodes.SUCCESS, "Token refund to employer failed");
 
